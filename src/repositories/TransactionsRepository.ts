0bf191162acbb351a97ac5453d32af28f800803e
gabrielmellooliveira <gabrielmello3011@gmail.com>
2020-04-25 18:51:36 -0300

    Create transaction functionality

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,29 +1,54 @@
 import Transaction from '../models/Transaction';
 
 interface Balance {
-  income: number;
-  outcome: number;
-  total: number;
+    income: number;
+    outcome: number;
+    total: number;
+}
+
+interface TransactionDTO {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
-  private transactions: Transaction[];
+    private transactions: Transaction[];
 
-  constructor() {
-    this.transactions = [];
-  }
+    constructor() {
+        this.transactions = [];
+    }
 
-  public all(): Transaction[] {
-    // TODO
-  }
+    public all(): Transaction[] {
+        return this.transactions;
+    }
 
-  public getBalance(): Balance {
-    // TODO
-  }
+    public getBalance(): Balance {
+        let income = 0;
+        let outcome = 0;
 
-  public create(): Transaction {
-    // TODO
-  }
+        this.transactions.forEach(transaction => {
+            if (transaction.type === 'income') {
+                income += transaction.value;
+            } else if (transaction.type === 'outcome') {
+                outcome += transaction.value;
+            }
+        });
+
+        return {
+            income,
+            outcome,
+            total: income - outcome,
+        };
+    }
+
+    public create({ title, value, type }: TransactionDTO): Transaction {
+        const transaction = new Transaction({ title, value, type });
+
+        this.transactions.push(transaction);
+
+        return transaction;
+    }
 }
 
 export default TransactionsRepository;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,32 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+    title: string;
+    value: number;
+    type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
-  private transactionsRepository: TransactionsRepository;
+    private transactionsRepository: TransactionsRepository;
 
-  constructor(transactionsRepository: TransactionsRepository) {
-    this.transactionsRepository = transactionsRepository;
-  }
+    constructor(transactionsRepository: TransactionsRepository) {
+        this.transactionsRepository = transactionsRepository;
+    }
 
-  public execute(): Transaction {
-    // TODO
-  }
+    public execute({ title, value, type }: RequestDTO): Transaction {
+        if (type !== 'income' && type !== 'outcome') {
+            throw Error('Invalid type of transaction');
+        }
+
+        const balance = this.transactionsRepository.getBalance();
+
+        if (type === 'outcome' && value > balance.total) {
+            throw Error('The outcome exceeded the total value');
+        }
+
+        return this.transactionsRepository.create({ title, value, type });
+    }
 }
 
 export default CreateTransactionService;
